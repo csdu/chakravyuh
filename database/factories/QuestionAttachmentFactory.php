6466ed807f281127936a49a12b304638750359d0
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2019-12-25 13:52:19 +0530

    implement changes suggested by @rumansaleem in questionAttachment factory

--- database/factories/QuestionAttachmentFactory.php
+++ database/factories/QuestionAttachmentFactory.php
@@ -8,32 +8,23 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Faker\Generator as Faker;
 
+// delete existing files
+Storage::deleteDirectory('attachments');
+
 $factory->define(QuestionAttachment::class, function (Faker $faker) {
-    $questionAttachments = [
-        [
-            'path' => 'factories/files/image.jpg',
-            'name' => 'image.jpg',
-            'type' => 'image',
-        ],
-        [
-            'path' => 'factories/files/video.mp4',
-            'name' => 'video.mp4',
-            'type' => 'video',
-        ],
-        [
-            'path' => 'factories/files/audio.mp3',
-            'name' => 'audio.mp3',
-            'type' => 'audio',
-        ],
+    $questionAttachment = [
+        'image' => new UploadedFile(database_path('factories/files/image.jpg'), 'image.jpg'),
+        'video' => new UploadedFile(database_path('factories/files/video.mp4'), 'video.mp4'),
+        'audio' => new UploadedFile(database_path('factories/files/audio.mp3'), 'audio.mp3'),
     ];
-    $questionAttachment = $faker->randomElement($questionAttachments);
+
+    $type = $faker->randomElement(['image', 'video', 'audio']);
 
     return [
-        'path' => function () use ($questionAttachment) {
-            $file = new UploadedFile(database_path($questionAttachment['path']), $questionAttachment['name']);
-            return Storage::putFile('attachments', $file);
+        'path' => function () use ($questionAttachment, $type) {
+            return Storage::putFile('attachments', $questionAttachment[$type]);
         },
-        'type' => $questionAttachment['type'],
+        'type' => $type,
         'question_id' => function () {
             return factory(Question::class)->create()->id;
         }
