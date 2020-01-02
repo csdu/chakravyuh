b62c1c5688e07d436d62c4ecf0325bbfcdc959d5
Yuvraj Sablania <sablaniayuvraj@gmail.com>
2020-01-02 13:15:44 +0530

    clean up and bugs fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
 <div class="sm:text-xs block ">
 
-    <div class="lg:w-1/2 md:w-3/4 w-4/5 absolute left-50 translate-n50 lg:top-40 top-50" style="font-family: 'Major Mono Display', monospace;">
+    <div class="lg:w-1/2 md:w-3/4 w-4/5 absolute left-50 translate-n50 lg:top-40 top-50 z-0" style="font-family: 'Major Mono Display', monospace;">
       <svg version="1.1" viewBox="0 0 600 600" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
         <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" font-size="5rem" fill="#ffdc34" >CHAKRAVYUH</text>
       </svg>
