0c98898c6bd9a5544a0806d8d80f04fdbeabe63e
Abhishek Kansal <abhishek.mca18.du@gmail.com>
2020-01-02 19:14:08 +0530

    update leaderboard to blade

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 Vue.component('timer',require('./components/countdownTimer.vue').default);
-Vue.component('leaderboard',require('./components/Leaderboard.vue').default);
 
 const app = new Vue({
     el: '#app',
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,34 @@ module.exports = {
       colors: {
         'primary' : '#ffdc34'
       },
+      inset: {
+        '-4': '-1.5rem',
+        '-8': '-3rem'
+      }
     }
   },
-  variants: {},
-  plugins: []
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'first'],
+    textColor: ['responsive', 'hover', 'focus', 'first'],
+    fontWeight: ['responsive', 'hover', 'focus', 'first'],
+    fontSize: ['responsive', 'first'],
+    textAlign: ['responsive', 'first'],
+    height: ['responsive', 'first'],
+    inset: ['responsive', 'first'],
+    position: ['responsive', 'first'],
+    borderWidth: ['responsive', 'first'],
+  },
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.bg-grad-gold-orange': {
+          background: `radial-gradient(circle, rgba(212, 175, 55, 1) 18%, rgba(195, 101, 34, 1) 90%)`,
+        }
+      }
+
+      addUtilities(newUtilities, {
+        variants: ['responsive', 'first']
+      })
+    }
+  ]
 }
