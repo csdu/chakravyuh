e8d70df9171722758faceab9e7df991e7a52a424
Ruman Saleem <ruman63@gmail.com>
2020-02-03 11:42:01 +0530

    audit npm packages

--- public/js/app.js
+++ public/js/app.js
@@ -17975,8 +17975,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/yuvraj/Documents/chakravyuh/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/yuvraj/Documents/chakravyuh/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /hdd/code/work/chakravyuh/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /hdd/code/work/chakravyuh/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
