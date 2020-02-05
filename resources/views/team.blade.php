caa801a21d9aab36f7df82056150c293c4472dac
abhinav2188 <abhinav.mca19.du@gmail.com>
2020-02-05 21:50:06 +0530

    team page changes

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -2,17 +2,20 @@
 
 @section('main')
 
-<div class="text-left md:w-3/4 w-11/12 mx-auto my-16 text-white flex flex-col">
+@php
+$arr1 = [1,3,5];
+$arr2= [2,4,6];
+shuffle($arr1);
+shuffle($arr2);
+@endphp
 
-  <span class="fixed z-n10 font-display self-center mt-48 opacity-25 md:font-4rem text-4xl ">Developers</span>
-  <h1 class="lg:text-3xl md:text-2xl text-xl border-b border-white ">Meet the Chakravyuh Developer Team</h1>
+<div class="text-left my-8 w-full text-white flex flex-col">
 
-  <div class="flex relative flex-col my-10 w-full">
-    <div class="flex flex-col ">
-      <h2 class="border-b lg:w-80 md:w-64 w-24 my-4 md:text-xl text-base">Team Head</h2>
-      <div class="w-full flex my-6">
-        <img class="lg:w-64 md:w-48 lg:h-64 md:h-48 w-32 h-32 rounded-sm bg-gray-900" src="https://avatars3.githubusercontent.com/u/25204498?s=400&v=4" alt="">
-        <div class="flex flex-col md:ml-16 ml-4">
+  <span class="fixed z-n10 font-display translateY-n50 self-end mt-48 opacity-10 md:font-6rem text-primary text-4xl ">DEVELOPERS</span>
+  {{-- <h1 class="lg:text-3xl md:text-2xl text-xl border-b border-white ">Meet the Chakravyuh Developer Team</h1> --}}
+  <div class="flex relative flex-col md:w-2/3 w-11/12 mx-auto">
+      <div class="w-full flex my-6 p-2 justify-end order-{{$arr1[0]}}">
+        <div class="flex flex-col md:mr-16 mr-4">
           <span class="md:text-lg text-sm">Ruman Saleem</span>
           <div class="flex">
             <a href="https://github.com/rumansaleem" class="my-2 mr-4">
@@ -23,9 +26,9 @@
             </a>
           </div>
         </div>
+        <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars3.githubusercontent.com/u/25204498?s=400&v=4" alt="">
       </div>
-      <h2 class="border-b lg:w-80 md:w-64 w-48 md:text-xl text-base my-4">Frontend Team</h2>
-      <div class="w-full flex p-2 my-6">
+      <div class="w-full flex p-2 my-6 order-{{$arr2[0]}}">
         <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars2.githubusercontent.com/u/24354587?s=400&v=4" alt="">
         <div class="flex flex-col md:ml-16 ml-4">
           <span class="md:text-lg text-sm">Abhishek Kansal</span>
@@ -39,24 +42,8 @@
           </div>
         </div>
       </div>
-      <div class="w-full flex p-2 my-6 justify-end">
+      <div class="w-full flex p-2 my-6 justify-end order-{{$arr1[1]}}">
         <div class="flex flex-col md:mr-16 mr-4">
-          <span class="md:text-lg text-sm">Abhinav Rastogi</span>
-          <div class="flex">
-            <a href="https://github.com/abhinav2188" class="my-2 mr-4">
-              @include('svg.github-icon')
-            </a>
-            <a href="https://www.linkedin.com/in/abhinav-rastogi-4706a0157/" class="my-2">
-              @include('svg.linkedin-icon')
-            </a>
-          </div>
-        </div>
-        <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars2.githubusercontent.com/u/56015300?s=400&v=4" alt="">
-      </div>
-      <h2 class="border-b lg:w-80 md:w-64 w-48 md:text-xl text-base my-4">Backend Team</h2>
-      <div class="w-full flex p-2 my-6">
-        <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars2.githubusercontent.com/u/27115069?s=460&v=4" alt="">
-        <div class="flex flex-col md:ml-16 ml-4">
           <span class="md:text-lg text-sm">Yuvraj Sablania</span>
           <div class="flex">
             <a href="https://github.com/yuvrajsab" class="my-2 mr-4">
@@ -67,23 +54,9 @@
             </a>
           </div>
         </div>
+        <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars2.githubusercontent.com/u/27115069?s=460&v=4" alt="">
       </div>
-      <div class="w-full flex p-2 my-6 justify-end">
-        <div class="flex flex-col md:mr-16 mr-4">
-          <span class="md:text-lg text-sm">Shubham Sagwan</span>
-          <div class="flex">
-            <a href="https://github.com/shubham-pal" class="my-2 mr-4">
-              @include('svg.github-icon')
-            </a>
-            <a href="#" class="my-2">
-              @include('svg.linkedin-icon')
-            </a>
-          </div>
-        </div>
-        <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars2.githubusercontent.com/u/55023076?s=460&v=4" alt="">
-      </div>
-      <h2 class="border-b lg:w-80 md:w-64 w-48 md:text-xl text-base my-4">Contributors</h2>
-      <div class="w-full flex p-2 my-6">
+      <div class="w-full flex p-2 my-6 order-{{$arr2[1]}}">
         <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900"
             src="https://avatars0.githubusercontent.com/u/29013078?s=460&v=4" alt="">
         <div class="flex flex-col md:ml-16 ml-4">
@@ -98,8 +71,36 @@
             </div>
         </div>
       </div>
-    </div>
-
+      <div class="w-full flex p-2 my-6 justify-end order-{{$arr1[2]}}">
+        <div class="flex flex-col md:mr-16 mr-4">
+          <span class="md:text-lg text-sm">Shubham Sagwan</span>
+          <div class="flex">
+            <a href="https://github.com/shubham-pal" class="my-2 mr-4">
+              @include('svg.github-icon')
+            </a>
+            <a href="#" class="my-2">
+              @include('svg.linkedin-icon')
+            </a>
+          </div>
+        </div>
+        <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars2.githubusercontent.com/u/55023076?s=460&v=4" alt="">
+      </div>
+      <div class="w-full flex p-2 my-6 order-{{$arr2[2]}}">
+        <img class="lg:w-48 md:w-32 lg:h-48 md:h-32 w-24 h-24 rounded-sm bg-gray-900" src="https://avatars2.githubusercontent.com/u/56015300?s=400&v=4" alt="">
+        <div class="flex flex-col  md:ml-16 ml-4">
+          <span class="md:text-lg text-sm">Abhinav Rastogi</span>
+          <div class="flex">
+            <a href="https://github.com/abhinav2188" class="my-2 mr-4">
+              @include('svg.github-icon')
+            </a>
+            <a href="https://www.linkedin.com/in/abhinav-rastogi-4706a0157/" class="my-2">
+              @include('svg.linkedin-icon')
+            </a>
+          </div>
+        </div>
+      </div>
   </div>
+
+</div>
 
 @endsection
